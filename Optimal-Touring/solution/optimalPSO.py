8c177a12cc76b44c31db16f83c5a2140b66a9a27
PranavGrandhi <pranav.grandhi@gmail.com>
2024-09-25 19:45:46 -0400

    Correct reward

--- Optimal-Touring/solution/optimalPSO.py
+++ Optimal-Touring/solution/optimalPSO.py
@@ -60,7 +60,6 @@ class Particle:
         self.best_value = -math.inf
         self.velocity = []
         self.initialize_routes()
-        self.best_routes = deepcopy(self.routes)
         self.evaluate()
 
     def initialize_routes(self):
@@ -78,82 +77,81 @@ class Particle:
 
     def evaluate(self):
         self.value = 0
-        self.feasible = True
+        self.total_reward = 0
+        self.feasible_routes = {}
         for day in self.routes:
             route = self.routes[day]
-            route_value, is_feasible = self.evaluate_route(route, day)
-            if not is_feasible:
-                self.feasible = False
-                break
-            self.value += route_value
-        if self.feasible:
-            if self.value > self.best_value:
-                self.best_value = self.value
-                self.best_routes = deepcopy(self.routes)
-            elif self.best_routes is None:
-                self.best_routes = deepcopy(self.routes)
+            route_reward, feasible_route = self.evaluate_route(route, day)
+            self.total_reward += route_reward
+            self.feasible_routes[day] = feasible_route
+        if self.total_reward > self.best_value:
+            self.best_value = self.total_reward
+            self.best_routes = deepcopy(self.routes)
 
-    def evaluate_route(self, route: List[int], day: int) -> Tuple[float, bool]:
+    def evaluate_route(self, route: List[int], day: int) -> Tuple[float, List[int]]:
         current_time = 0
-        total_value = 0
+        total_reward = 0
+        feasible_route = []
         for i, site_id in enumerate(route):
             site = self.sites[site_id]
-            tw = site.time_windows[day]
+            tw = site.time_windows.get(day)
+            if not tw:
+                continue
             begin_time = tw['begin_hour'] * 60
             end_time = (tw['end_hour'] + 1) * 60 - 1
 
-            # Travel time from previous site (except for the first site)
-            if i > 0:
-                prev_site = self.sites[route[i - 1]]
+            # Travel time from previous site
+            if feasible_route:
+                prev_site = self.sites[feasible_route[-1]]
                 travel_time = calculate_travel_time(prev_site, site)
                 current_time += travel_time
             else:
-                # Assume we can start at any time
                 current_time = 0
 
-            # Wait until the site opens
-            if current_time < begin_time:
-                waiting_time = begin_time - current_time
-                current_time = begin_time
-            else:
-                waiting_time = 0
+            arrival_time = current_time
 
-            # Time spent at the site
-            total_delay = waiting_time + site.desired_time
+            # Wait until the site opens
+            waiting_time = max(begin_time - arrival_time, 0)
+            arrival_time += waiting_time
+
+            # Time needed at site
+            departure_time = arrival_time + site.desired_time
 
             # Check if we can finish visiting before the site closes
-            if current_time + site.desired_time > end_time:
-                return 0, False
+            if departure_time <= end_time:
+                # We can visit this site
+                total_reward += site.value
+                feasible_route.append(site_id)
+                current_time = departure_time
+            else:
+                # Cannot visit this site within its time window
+                continue
 
-            # Update current time
-            current_time += site.desired_time
-
-            total_value += site.value
-
-        return total_value, True
+        return total_reward, feasible_route
 
     def update_velocity(self, global_best):
         self.velocity = []
-        if self.best_routes:
-            for day in self.routes:
-                route = self.routes[day]
-                best_route = self.best_routes[day]
-                common_sites = set(route).intersection(set(best_route))
-                for site_id in common_sites:
-                    i = route.index(site_id)
-                    j = best_route.index(site_id)
-                    if i != j:
-                        self.velocity.append(('swap', day, i, j))
-                sites_to_add = [site for site in best_route if site not in route]
-                for site_id in sites_to_add:
-                    self.velocity.append(('add', day, site_id))
-                sites_to_remove = [site for site in route if site not in best_route]
-                for site_id in sites_to_remove:
-                    self.velocity.append(('remove', day, site_id))
-
+        # Move towards personal best
         for day in self.routes:
             route = self.routes[day]
-            global_route = global_best.routes[day]
+            best_route = self.best_routes.get(day, [])
+            common_sites = set(route).intersection(set(best_route))
+            for site_id in common_sites:
+                i = route.index(site_id)
+                j = best_route.index(site_id)
+                if i != j:
+                    self.velocity.append(('swap', day, i, j))
+            sites_to_add = [site for site in best_route if site not in route]
+            for site_id in sites_to_add:
+                self.velocity.append(('add', day, site_id))
+            sites_to_remove = [site for site in route if site not in best_route]
+            for site_id in sites_to_remove:
+                self.velocity.append(('remove', day, site_id))
+
+        # Move towards global best
+        for day in self.routes:
+            route = self.routes[day]
+            global_route = global_best.best_routes.get(day, [])
             common_sites = set(route).intersection(set(global_route))
             for site_id in common_sites:
                 i = route.index(site_id)
@@ -184,21 +182,47 @@ class Particle:
                     self.routes[day].remove(site_id)
         self.velocity = []
 
-    def mutate(self):
-        # Randomly swap two sites in a day
-        day = random.choice(list(self.routes.keys()))
-        if len(self.routes[day]) > 1:
-            i, j = random.sample(range(len(self.routes[day])), 2)
-            self.routes[day][i], self.routes[day][j] = self.routes[day][j], self.routes[day][i]
-        # Randomly move a site from one day to another
-        day_from, day_to = random.sample(list(self.routes.keys()), 2)
-        if self.routes[day_from]:
-            site_id = random.choice(self.routes[day_from])
-            site = self.sites[site_id]
-            if day_to in site.open_days:
-                self.routes[day_from].remove(site_id)
-                self.routes[day_to].append(site_id)
-                random.shuffle(self.routes[day_to])
+    def mutate(self, iteration, max_iterations):
+        # Compute mutation_rate
+        mutation_rate = 1 - (iteration / max_iterations)
+        MAX_MUTATIONS = 5  # Adjust as needed
+        num_mutations = max(1, int(mutation_rate * MAX_MUTATIONS))
+
+        for _ in range(num_mutations):
+            # Randomly select a mutation type
+            mutation_type = random.choice(['swap_within_day', 'swap_between_days', 'move_between_days', 'reverse_segment'])
+
+            if mutation_type == 'swap_within_day':
+                day = random.choice(list(self.routes.keys()))
+                if len(self.routes[day]) > 1:
+                    i, j = random.sample(range(len(self.routes[day])), 2)
+                    self.routes[day][i], self.routes[day][j] = self.routes[day][j], self.routes[day][i]
+
+            elif mutation_type == 'swap_between_days':
+                day1, day2 = random.sample(list(self.routes.keys()), 2)
+                if self.routes[day1] and self.routes[day2]:
+                    idx1 = random.randrange(len(self.routes[day1]))
+                    idx2 = random.randrange(len(self.routes[day2]))
+                    site1 = self.routes[day1][idx1]
+                    site2 = self.routes[day2][idx2]
+                    if day2 in self.sites[site1].open_days and day1 in self.sites[site2].open_days:
+                        self.routes[day1][idx1], self.routes[day2][idx2] = site2, site1
+
+            elif mutation_type == 'move_between_days':
+                day_from, day_to = random.sample(list(self.routes.keys()), 2)
+                if self.routes[day_from]:
+                    site_id = random.choice(self.routes[day_from])
+                    site = self.sites[site_id]
+                    if day_to in site.open_days:
+                        self.routes[day_from].remove(site_id)
+                        self.routes[day_to].append(site_id)
+
+            elif mutation_type == 'reverse_segment':
+                day = random.choice(list(self.routes.keys()))
+                if len(self.routes[day]) > 2:
+                    start_idx = random.randint(0, len(self.routes[day]) - 2)
+                    end_idx = random.randint(start_idx + 1, len(self.routes[day]) - 1)
+                    self.routes[day][start_idx:end_idx+1] = list(reversed(self.routes[day][start_idx:end_idx+1]))
 
 class PSO:
     def __init__(self, sites: Dict[int, Site], max_day: int, num_particles: int, num_iterations: int):
@@ -207,33 +231,31 @@ class PSO:
         self.num_particles = num_particles
         self.num_iterations = num_iterations
         self.particles = [Particle(sites, max_day) for _ in range(num_particles)]
-        feasible_particles = [p for p in self.particles if p.feasible]
-        if feasible_particles:
-            self.global_best = max(feasible_particles, key=lambda p: p.best_value)
-        else:
-            self.global_best = self.particles[0]
+        self.global_best = max(self.particles, key=lambda p: p.best_value)
 
     def run(self):
         for iteration in range(self.num_iterations):
             for particle in self.particles:
                 particle.update_velocity(self.global_best)
                 particle.apply_velocity()
-                particle.mutate()
+                particle.mutate(iteration, self.num_iterations)
                 particle.evaluate()
-                if particle.feasible and particle.best_value > self.global_best.best_value:
+                if particle.best_value > self.global_best.best_value:
                     self.global_best = deepcopy(particle)
             print(f"Iteration {iteration + 1}/{self.num_iterations}, Best Value: {self.global_best.best_value}")
 
     def write_solution(self, filename='result.txt'):
         with open(filename, 'w') as f:
-            for day in sorted(self.global_best.routes.keys()):
-                route = self.global_best.routes[day]
+            for day in sorted(self.global_best.feasible_routes.keys()):
+                route = self.global_best.feasible_routes[day]
                 if not route:
                     continue
                 current_time = 0
                 for i, site_id in enumerate(route):
                     site = self.sites[site_id]
-                    tw = site.time_windows[day]
+                    tw = site.time_windows.get(day)
+                    if not tw:
+                        continue
                     begin_time = tw['begin_hour'] * 60
                     end_time = (tw['end_hour'] + 1) * 60 - 1
 
@@ -243,10 +265,8 @@ class PSO:
                         travel_time = calculate_travel_time(prev_site, site)
                         current_time += travel_time
                     else:
-                        # Assume we can start at any time
                         current_time = 0
 
-                    # Arrival time at the site
                     arrival_time = current_time
 
                     # Wait until the site opens
